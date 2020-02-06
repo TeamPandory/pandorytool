7c8dab36d21342dd4c6512071d23646efc348a39
dajoho <dh@dajoho.com>
2020-02-06 19:21:07 +0100

    Added support for #6

--- winbuild.sh
+++ winbuild.sh
@@ -3,3 +3,7 @@ mkdir -p build
 cd build || exit;
 cmake -G "MinGW Makefiles" -DCMAKE_SH="CMAKE_SH-NOTFOUND" ..
 cmake --build .
+
+if [ -x "$(command -v upx)" ]; then
+  upx -9 build/pandory.exe
+fi
