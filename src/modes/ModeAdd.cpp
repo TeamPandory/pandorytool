4888b203e7392b4aed430e956f9865ae3348591b
emuchicken <45140115+emuchicken@users.noreply.github.com>
2020-02-08 09:35:48 +0900

    Update ModeAdd.cpp

--- src/modes/ModeAdd.cpp
+++ src/modes/ModeAdd.cpp
@@ -228,8 +228,8 @@ void ModeAdd::generateMcGamesMeta(tinyxml2::XMLElement *sourceGame, std::string 
 		if (shortSystemName == "MAME19") {
 		emutype = 19;}
 	
- // save incase i fck up std::string emuString = pad(std::to_string(emutype), 3, '0');
-    std::string emuString = pad(std::to_string(emutype),'0');
+std::string emuString = pad(std::to_string(emutype), , '0');
+   
     std::string desc = extractXMLText(sourceGame->FirstChildElement("desc"));
     std::string relativeRomPath = Fs::basename(extractXMLText(sourceGame->FirstChildElement("path")));
     std::string dateString = extractXMLText(sourceGame->FirstChildElement("releasedate"));
