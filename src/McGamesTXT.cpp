f1905f65bcca599f317355fd721c1f6029874783
dajoho <dh@dajoho.com>
2020-02-08 15:12:54 +0100

    Fixed the handbottom

--- src/McGamesTXT.cpp
+++ src/McGamesTXT.cpp
@@ -5,7 +5,7 @@ void McGamesTXT::generate(std::string filename) {
     std::ofstream fp;
     fp.open(filename);
 
-    fp << "display_name\t" << romName << std::endl;
+    fp << "display_name\t" << romFileName << std::endl;
     fp << "loading_time\t" << emulatorLoad << std::endl;
     fp << "emulator_type\t" << emulatorName << std::endl;
     fp << "genre\t" << "0" << std::endl;
