458dcb838d28064840ed3bfc02e36f315692e757
dajoho <dh@dajoho.com>
2020-03-19 23:18:01 +0100

    more build stuff

--- fedora-winbuild32.sh
+++ fedora-winbuild32.sh
@@ -8,7 +8,7 @@ if [ -x "$(command -v upx)" ]; then
   upx -9 pandory.exe
 fi
 
-cp -Rv controls/ build-win32/;
+cp -Rv ../controls/ ./;
 
 ZIPFILE="pandory-"$(git rev-parse HEAD)".zip"
 rm -f $ZIPFILE;
