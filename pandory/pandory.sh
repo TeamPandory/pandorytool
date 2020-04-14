8dbffbb2bdf3f99029e0ad4564baa59368c3cae1
dajoho <dh@dajoho.com>
2020-04-14 21:07:39 +0200

    Fixed "broken" sticks that didn't work in the beta-release

--- pandory/pandory.sh
+++ pandory/pandory.sh
@@ -3,24 +3,28 @@
 while true; do
 
 if [ -f "/storage/external_storage/sda1/pandory.sh" ]; then 
+	cd /storage/external_storage/sda1/;
 	busybox ash /storage/external_storage/sda1/pandory.sh; 
 	exit 0;
 fi
 
 
 if [ -f "/storage/external_storage/sdb1/pandory.sh" ]; then 
+	cd /storage/external_storage/sdb1/;
 	busybox ash /storage/external_storage/sdb1/pandory.sh; 
 	exit 0;
 fi
 
 
 if [ -f "/storage/external_storage/sda/pandory.sh" ]; then 
+	cd /storage/external_storage/sda/;
 	busybox ash /storage/external_storage/sda/pandory.sh; 
 	exit 0;
 fi
 
 
 if [ -f "/storage/external_storage/sdb/pandory.sh" ]; then 
+	cd /storage/external_storage/sdb/;
 	busybox ash /storage/external_storage/sdb/pandory.sh; 
 	exit 0;
 fi
