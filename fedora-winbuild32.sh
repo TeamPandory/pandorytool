60e16f238d19a25c7a9ef5aee83a81a033d1181c
dajoho <dh@dajoho.com>
2020-04-18 21:46:52 +0200

    Updated buildscripts to retrieve sticks & controls

--- fedora-winbuild32.sh
+++ fedora-winbuild32.sh
@@ -8,8 +8,22 @@ if [ -x "$(command -v upx)" ]; then
   upx -9 pandory.exe
 fi
 
-cp -Rv ../controls/ ./;
+rm -rf sticks;
+mkdir sticks;
+cd sticks;
+wget https://github.com/emuchicken/pandorytool/archive/stick-jailbreak.tar.gz;
+wget https://github.com/emuchicken/pandorytool/archive/stick-backup.tar.gz;
+cd ..;
+
+rm -rf controls.tar.gz;
+rm -rf controls;
+rm -rf pandorytool-controls;
+wget https://github.com/emuchicken/pandorytool/archive/controls.tar.gz;
+tar xvf controls.tar.gz;
+mv pandorytool-controls/ controls/;
+
+cp ../*.md .;
 
 ZIPFILE="pandory-"$(git rev-parse HEAD)".zip"
 rm -f $ZIPFILE;
-zip -jv -r $ZIPFILE pandory.exe controls ../README.md ../HISTORY.md ;
+zip -v -r $ZIPFILE pandory.exe controls/ sticks/ *.md;
