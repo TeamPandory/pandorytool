52437f7f8e5144f6be351928d0833a748c0412f4
dajoho <dh@dajoho.com>
2020-04-19 09:21:18 +0200

    Various small fixes

--- src/PandoryTool.cpp
+++ src/PandoryTool.cpp
@@ -39,7 +39,6 @@ int PandoryTool::main() {
     std::cout << "== Pandorytool has been worked on with blood, sweat and tears. If you would like us to continue " << std::endl;
     std::cout << "== work on this tool, please consider grabbing us a coffee at https://www.buymeacoffee.com/CKZbiXa." << std::endl;
     std::cout << std::endl;
-    std::cout << " * Various modded ROM controls contributed by: filepirate" << std::endl;
 #endif
     std::cout << "---------------------------------------------------------------------------------------------------" << std::endl;
 
--- src/StickExtractor.cpp
+++ src/StickExtractor.cpp
@@ -1,6 +1,8 @@
 #include "StickExtractor.h"
 #include "libarchive.h"
 #include "Fs.h"
+#include <iostream>
+#include <algorithm>
 
 int StickExtractor::exractToFolder(std::string &stickType, std::string &targetFolder) {
     std::string curDir = Fs::getCurrentPath();
@@ -14,6 +16,12 @@ int StickExtractor::exractToFolder(std::string &stickType, std::string &targetFo
 
     std::string extractedFolder = tmpDir+"pandorytool-stick-"+stickType;
     chdir(curDir.c_str());
-    Fs::copyRecursive(extractedFolder, targetFolder);
+    std::string cmd = extractedFolder + "/*.* " + targetFolder; 
+    std::replace(cmd.begin(),cmd.end(),'/','\\'); 
+    // TODO: cross-platform copy. locked to windows temporarily
+    //std::cout << cmd << std::endl;
+    std::string execCmd = "xcopy /E " + cmd;
+    system(execCmd.c_str());
+    //Fs::copyRecursive(extractedFolder, targetFolder);
     return 0;
 }
