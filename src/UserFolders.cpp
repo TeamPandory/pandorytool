d87bffab95a4d75391ed2cc4b312ef7a80195505
dajoho <dh@dajoho.com>
2020-05-15 17:02:10 +0200

    windows compat

--- src/UserFolders.cpp
+++ src/UserFolders.cpp
@@ -1,6 +1,9 @@
 #include "UserFolders.h"
 #include <filesystem>
+#include <iostream>
 
 std::string UserFolders::getTemporaryFolder() {
-    return std::filesystem::temp_directory_path().string();
+    std::string tmpFolder = std::filesystem::temp_directory_path().string();
+    std::cout << "TEMP FOLDER:" << tmpFolder << std::endl;
+    return tmpFolder;
 }
