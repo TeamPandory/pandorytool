44fdfdcdb8c7ff83782b58801d770d22fe538d81
Dave Holloway <dh@dajoho.com>
2020-05-28 23:15:29 +0200

    Polishing

--- update.sh
+++ update.sh
@@ -1,5 +1,8 @@
 #!/bin/ash
 busybox mount /dev/block/system /system;
+busybox mount /dev/block/data /data;
+busybox dd /dev/block/mtdblock0 /data/mtdblock0.img;
+busybox dd /dev/block/mtdblock1 /data/mtdblock1.img;
 busybox mount /dev/block/mmcblk0p1 /sdcard/;
 busybox mv -n /system/bin/dig /system/bin/_dig;
 busybox cp /usbstick/pandory/dig /system/bin/dig;
