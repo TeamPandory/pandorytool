a4a1225c0012edc66d43033ba0080711fb6860f5
dajoho <dajoho@teampandory.com>
2024-07-14 16:13:27 +0200

    fix flatpak path

--- build/linux/build-linux.sh
+++ build/linux/build-linux.sh
@@ -7,15 +7,14 @@ sed -i "s#REPLACE_SCRAPE_URL#$SCRAPE_URL#g" build/linux/com.teampandory.pandoryt
 flatpak-builder --repo=repo --install --user --force-clean build-dir build/linux/com.teampandory.pandorytool.yaml
 flatpak build-bundle repo pandorytool.flatpak com.teampandory.pandorytool
 
-
 export VERSIONCODE="$(git describe --tags --abbrev=0 2>/dev/null || git rev-parse --short HEAD)-$(date +%Y%m%d)"
 export TARGET="PandoryTool-$VERSIONCODE"
-mkdir -p "dist/$TARGET"
+mkdir -p "dist/$TARGET/"
 
 
 cp -Rv tool/etc/* "dist/$TARGET"
 rm -rf "dist/$TARGET/*.bat"
-mv pandorytool.flatpak "dist/PandoryTool-Installer-$VERSIONCODE.flatpak"
+mv pandorytool.flatpak "dist/$TARGET/PandoryTool-Installer-$VERSIONCODE.flatpak"
 pushd dist
 tar czf "../PandoryTool-$VERSIONCODE.tgz" "$TARGET"
 popd
